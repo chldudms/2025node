69095618dde7a08e36b9e44b7f1ea20c4cf7c49d
chldudms <yueun0337@gmail.com>
2025-03-21 09:04:43 +0900

    post요청

--- app.js
+++ app.js
@@ -23,9 +23,18 @@ const express = require('express');
 const app = express();
 
 // 루트 경로('/')에 GET 요청이 왔을 때 'Hello, World!' 응답을 보냅니다.
-app.get('/', (req, res) => {
-    res.send('bye, world!\n');
+// app.get('/', (req, res) => {
+//     res.send('bye, world!\n');
+// });
+
+
+app.get('/swag', (req, res) => {
+    res.send('get, swag!\n');
 });
+
+app.post('/swag', (req, res) => {
+    res.send('post swag!\n');
+}); 
 
 // 서버가 포트 3000에서 요청을 대기합니다.
 app.listen(3000, () => {
