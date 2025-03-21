77af08c457287a4e98f011f0091b8c41f087e231
chldudms <yueun0337@gmail.com>
2025-03-21 09:19:27 +0900

    params  요청

--- app.js
+++ app.js
@@ -37,6 +37,10 @@ app.post('/swag', (req, res) => {
     res.send(req.body);
 }); 
 
+app.get('/swag/:person', (req, res) => {
+    res.send(req.params.person);
+}); 
+
 // 서버가 포트 3000에서 요청을 대기합니다.
 app.listen(3000, () => {
     console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
