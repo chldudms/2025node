9897dbd2b30e988fd8e4a7a5ebc633e03feb9fa5
chldudms <yueun0337@gmail.com>
2025-03-28 09:59:56 +0900

    .env

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express();
 app.set('view engine','ejs');
 //dirname:현재 파일이 속한 절대 경로
 //path.join을 사용하면 운영체제와 상관없이 경로 구분자(/,\)을 알아서 잡아준다. 
-app.set('views', path.join(__dirname,'views'));
+//절대경로와 타입경로를 연결해준다.
+app.set('views', path.join(__dirname,'views'));  
 
 console.log(path.join(__dirname,'views'));
 
--- app2.js
+++ app2.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const path = require("path");  // path 모듈 추가
+const mysql = require('mysql2');
+const { createConnection } = require("net");
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
 
 travelList = ['뉴욕', '파리','서울','도쿄'];
 
@@ -10,6 +21,13 @@ app.set('view engine','ejs');
 //path.join을 사용하면 운영체제와 상관없이 경로 구분자(/,\)을 알아서 잡아준다. 
 
 app.set('views', path.join(__dirname,'views'));
+
+db.connect(err=> {
+    if(err){
+        console.error('MySQL 연결 실패',err)
+    }
+    console.log('mysql에 연결되었습니다.  ')
+})
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -22,3 +40,4 @@ app.get('/travel',(req,res)=>{
 app.listen(3000, () => {
     console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
 });
+
